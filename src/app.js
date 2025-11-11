3b905a48197cd2f8a793451eab0bb15f5f96ac05
farha-f <farhatm027@gmail.com>
2025-11-11 16:00:16 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
-app.use('/test',(req,res)=>{
-    res.send('hello from server');
-});
-app.use('/data',(req,res)=>{
-    res.send("Here is some data from data");
+app.use('/data',(req,res, next)=>{
+  
+    res.send({fname:"John", lname:"Doe"});
+    console.log("Request received");
+      next();
+    
+},(req, res)=>{
+res.send("Hello from callback");
+console.log("Callback executed");
 })
+// app.get('/data',(req,res)=>{
+//     res.send({fname:"John", lname:"Doe"});
+// });
+// app.get('/data/:userId/:password',(req,res)=>{
+//     console.log(req.params);
+//     res.send({fname:"John", lname:"Doe"});
+// });
+// app.post('/data',(req,res)=>{
+//     res.send("Data Posted Successfully");   
+// });
+// app.use('/test',(req,res)=>{
+//     res.send('hello from server');
+// });
+// app.use('/data',(req,res)=>{
+//     res.send("Here is some data from data");
+// })
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
